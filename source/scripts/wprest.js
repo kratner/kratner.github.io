50db94e558019b878a79deca7dfdf0366ee6dae3
Keith Ratner <keith.ratner@mirumagency.com>
2016-03-27 18:34:28 -0700

    Add colors and mixing, additional styling

--- source/scripts/wprest.js
+++ source/scripts/wprest.js
@@ -1,7 +1,7 @@
-(function (window, document) {
+(function(window, document) {
     'use strict';
     let init = () => {
-    	//alert('wprest testing');
+        //alert('wprest testing');
     };
     $(document).ready(init);
 })(window, document);
