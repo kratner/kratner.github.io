63a76beabcf9cb1c2a5334da1c3b0889d0041a3b
Ratner, Keith <keith.ratner@mirumagency.com>
2016-04-01 16:36:52 -0700

    Remove superfluous code

--- js/main/main.js
+++ js/main/main.js
@@ -74,34 +74,21 @@
     var init = function init() {
         Controls.initializeNavControl();
         var api = {
-            //dataType: 'jsonp',
             uri: 'http://www.keithratner.com/?wpapi=get_posts&dev=1&id=2063',
             root: 'http://www.keithratner.com',
             pageid: '2063'
         },
-
-        //model = window.Core.Model(),
-        renderPost = function renderPost(post) {
+            renderPost = function renderPost(post) {
             console.log(post);
         },
             getPageById = function getPageById(id) {
             var url = api.root + '/wp-json/wp/v2/pages/' + api.pageid;
-            console.log(url);
             $.ajax({
                 crossDomain: true,
                 type: 'GET',
-                //headers: {'Access-Control-Allow-Origin': '*'},
-                //jsonpCallback: 'jsonhandler',
-                //contentType: 'application/json; charset=utf-8',
-                async: false,
-                //jsonp: 'callback',
-                //url: api.root + '?wpapi=get_posts&dev=1&id=' + id,
+                //async: false,
                 url: url,
                 dataType: 'json'
-                // jsonp: 'jsonp'
-                // success: function(data) {
-                //     console.log(data);
-                // }
             }).then(function (post, textStatus, jqXHR) {
                 renderPost(post);
             });
--- source/scripts/main/main.js
+++ source/scripts/main/main.js
@@ -4,33 +4,21 @@
     let init = () => {
         Controls.initializeNavControl();
         let api = {
-                //dataType: 'jsonp',
                 uri: 'http://www.keithratner.com/?wpapi=get_posts&dev=1&id=2063',
                 root: 'http://www.keithratner.com',
                 pageid: '2063'
             },
-            //model = window.Core.Model(),
             renderPost = (post) => {
                 console.log(post);
             },
             getPageById = id => {
                 let url = api.root + '/wp-json/wp/v2/pages/' + api.pageid;
-                console.log(url);
                 $.ajax({
                     crossDomain: true,
                     type: 'GET',
-                    //headers: {'Access-Control-Allow-Origin': '*'},
-                    //jsonpCallback: 'jsonhandler',
-                    //contentType: 'application/json; charset=utf-8',
-                    async: false,
-                    //jsonp: 'callback',
-                    //url: api.root + '?wpapi=get_posts&dev=1&id=' + id,
+                    //async: false,
                     url: url,
                     dataType: 'json'
-                    // jsonp: 'jsonp'
-                    // success: function(data) {
-                    //     console.log(data);
-                    // }
                 }).then((post, textStatus, jqXHR) => {
                     renderPost(post);
                 });
